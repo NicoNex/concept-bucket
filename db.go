a048ff5617327f28a9b48987f2437180ccf66881
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2020-05-19 14:49:19 +0200

    Bug fix.

--- db.go
+++ db.go
@@ -14,10 +14,14 @@ func (a Archive) Put(key string, c Concept) error {
 	var enc = gob.NewEncoder(&buf)
 
 	err := enc.Encode(c)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	db, err := bitcask.Open(string(a))
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	defer db.Close()
 
 	return db.Put([]byte(key), buf.Bytes())
@@ -29,8 +33,20 @@ func (a Archive) Get(key string) (Concept, error) {
 	var dec = gob.NewDecoder(&buf)
 
 	db, err := bitcask.Open(string(a))
-	if err != nil { return Concept{}, err }
+	if err != nil {
+		return c, err
+	}
 	defer db.Close()
+
+	b, err := db.Get([]byte(key))
+	if err != nil {
+		return c, err
+	}
+
+	_, err = buf.Write(b)
+	if err != nil {
+		return c, err
+	}
 
 	err = dec.Decode(&c)
 	return c, err
